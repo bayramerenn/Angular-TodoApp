19a4940742a3644277b5164584c399a65b21d3a2
BEren <bayram.eren@outlook.com.tr>
2021-03-16 00:28:33 +0300

    the end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Model, TodoItem } from './model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoApp';
+  model = new Model();
+  user = 'Bayram';
+  isDisplay = true;
+  
+
+  getName(){
+    return this.model.user;
+  }
+
+  getItems(){
+    if(this.isDisplay){
+      return this.model.items;
+    }
+    return this.model.items.filter(f => f.action === false);
+  }
+
+  addItem(value){
+    if(value != "")
+    this.model.items.push(new TodoItem(value,false));
+  }
 }
